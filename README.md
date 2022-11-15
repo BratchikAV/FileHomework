9d5b35634a7e66f4d20268ab1183f5bfa0795049
Bratchik Andrey <homolupus@inbox.ru>
2022-11-15 14:13:18 +0300

    Third exersize

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Initial commit - первый коммит (что дано к заданию)
 2. First exersize - решение задачи № 1
 3. Second exersize - решение  задачи № 2
+4. Third exersize - решение задачи № 3
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 with open("КулинарнаяКнига.txt", "rt", encoding="utf-8") as file:
     cook_book = {}
     for x in file:
@@ -29,4 +31,33 @@ def get_shop_list_by_dishes(dishes, person_count):
     print(wishlist)
 
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+import os
+
+current = os.getcwd()
+file_name_1 = "firstFile.txt"
+file_name_2 = "secondFile.txt"
+
+with open(file_name_1, "rt", encoding="utf-8") as file_1:
+    lines1 = 0
+    for _ in file_1:
+        lines1 += 1
+
+with open(file_name_2, "rt", encoding="utf-8") as file_2:
+    lines2 = 0
+    for _ in file_2:
+        lines2 += 1
+print(lines2)
+
+with open("thirdFile.txt", "wt", encoding="utf-8") as file_3:
+    file_3.write(f"{os.path.basename(file_name_2)} \n")
+    file_3.write(f"{str(lines2)} \n")
+    lines_2 = open(file_name_2, "rt", encoding="utf-8").readlines()
+    for line in lines_2:
+        file_3.write(f"{line.strip()} \n")
+    file_3.write(f"{os.path.basename(file_name_1)} \n")
+    file_3.write(f"{str(lines1)} \n")
+    lines_1 = open(file_name_1, "rt", encoding="utf-8").readlines()
+    for line in lines_1:
+        file_3.write(f"{line.strip()} \n")
 
